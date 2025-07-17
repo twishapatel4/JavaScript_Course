4ead1058ad00069d0cc8e3371686ee2f67bcdad2
Twisha Patel <twisha@techeniac.com>
2025-07-17 19:00:16 +0530

    Feat: First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+ let js='amazing';
+// if(js==='amazing'){
+//     alert("JS is Amazing!!");
+// }
+console.log(40+40-24/12);
+let country="India";
+let continent="Asia";
+let population=32;
+console.log(country);
+console.log(population);
+console.log(continent);
+
+let jsIsFun=true;
+console.log(jsIsFun);
+console.log(typeof true);
+console.log(typeof jsIsFun);
+jsIsFun="twi";
+console.log(typeof jsIsFun);
+let isIsland=false;
+let language;
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+language='hindi';
+const countryy="India";
+countryy="Bharat";
+// basic Operators
